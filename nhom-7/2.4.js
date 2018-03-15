661a43769bc0f7efb8e55555bc6227cffc37de36
Dung Nguyen <nguyenmanhdungtk@gmail.com>
2018-03-15 18:03:39 +0700

    update content

--- nhom-7/2.4.js
+++ nhom-7/2.4.js
@@ -10,6 +10,20 @@ var escapeBtn = document.getElementsByClassName("escape-btn")[0];
 // answer counter
 var countAnswer1 = 0;
 var countAnswer2 = 0;
+// trial
+var flipper = document.getElementsByClassName("flipper")[0];
+var isFront = true;
+
+flipper.addEventListener("click", function () {
+    if (isFront) {
+        this.style.transform = "rotateY(180deg)";
+    } else {
+        this.style.transform = "rotateY(0deg)";
+    }
+    this.children[0].classList.toggle("d-none");
+    this.children[1].classList.toggle("d-none");
+    isFront = !isFront;
+});
 
 main();
 
@@ -17,40 +31,40 @@ function main() {
     takeQuestion();
     // set time out after finishing lesson
     setInterval(function () {
-        if (countAnswer2 == 3 && countAnswer1 == 3) homeLink.click();
-    }, 2500)
-    // add event listeners for choices
+        if (countAnswer2 === rightAnswers2.length && countAnswer1 === rightAnswers1.length) homeLink.click();
+    }, 2500);
+    // add event for choices
     for (var i = 0; i < choices.length; i++) {
         choices[i].addEventListener("click", function () {
             if (countAnswer1 < questions1.length) {
                 if (this.getAttribute("alt") === rightAnswers1[countAnswer1].textContent) {
-                    progress.style.width = (String)("16.67" * (countAnswer1 + countAnswer2 + 1) + "%");
+                    progress.style.width = (String)((100 / (rightAnswers2.length + rightAnswers1.length)) * (countAnswer1 + countAnswer2 + 1) + "%");
                     countAnswer1++;
                     // move to next question
                     takeQuestion();
                 } else {
-                    rightAnswers1[countAnswer1].parentNode.classList.add("shaking");
+                    rightAnswers1[countAnswer1].parentElement.classList.add("shaking");
                 }
                 // change opacity of choice
-                for (var i = 0; i < choices.length; i++) {
-                    if (choices[i].id == this.id) this.style.opacity = "0.4";
-                    else choices[i].style.opacity = "1";
+                for (var j = 0; j < choices.length; j++) {
+                    if (choices[j].id === this.id) this.style.opacity = "0.4";
+                    else choices[j].style.opacity = "1";
                 }
             }
-        })
+        });
         choices[i].addEventListener("mouseover", function () {
             this.style.boxShadow = "0 2px 9px rgba(0,0,0,.7)";
-        })
+        });
         choices[i].addEventListener("mouseout", function () {
             this.style.boxShadow = "0 2px 9px rgba(0,0,0,.1)";
         })
     }
-    // add event listeners for input
+    // add event for input
     for (var i = 0; i < input.length; i++) {
         input[i].addEventListener("keyup", function (event) {
-            if (event.keyCode == 13 && countAnswer2 < questions2.length) {
+            if (event.keyCode === 13 && countAnswer2 < questions2.length) {
                 if (this.value === rightAnswers2[countAnswer2].getAttribute("alt")) {
-                    progress.style.width = (String)("16.67" * (countAnswer1 + countAnswer2 + 1) + "%");
+                    progress.style.width = (String)((100 / (rightAnswers2.length + rightAnswers1.length)) * (countAnswer1 + countAnswer2 + 1) + "%");
                     countAnswer2++;
                     // move to next question
                     takeQuestion();
@@ -60,7 +74,7 @@ function main() {
             }
         })
     }
-    // add event listener for escape button 
+    // add event for escape button
     escapeBtn.addEventListener("click", function () {
         homeLink.click();
     })
@@ -69,7 +83,7 @@ function main() {
 // move to next type of question
 function nextQuestion(questions_1, questions_2, countAnswer_1, countAnswer_2) {
     for (var i = 0; i < questions_1.length; i++) {
-        if (i == countAnswer_1) {
+        if (i === countAnswer_1) {
             questions_1[i].classList.remove("d-none");
             if (countAnswer_2 > 0) questions_2[countAnswer_2 - 1].classList.add("d-none");
         }
@@ -79,7 +93,7 @@ function nextQuestion(questions_1, questions_2, countAnswer_1, countAnswer_2) {
 
 // take question for user
 function takeQuestion() {
-    if (randomQuestion() == 1 && countAnswer1 < questions1.length) nextQuestion(questions1, questions2, countAnswer1, countAnswer2);
+    if (randomQuestion() === 1 && countAnswer1 < questions1.length) nextQuestion(questions1, questions2, countAnswer1, countAnswer2);
     else if (countAnswer2 < questions2.length) nextQuestion(questions2, questions1, countAnswer2, countAnswer1);
     else nextQuestion(questions1, questions2, countAnswer1, countAnswer2);
 }
