1cae13b246215e50360a05e92da935396b63de78
trinhtl <tonglytrinh@gmail.com>
2018-03-17 14:59:49 +0700

    fix giao dien

--- nhom-5/js/text.js
+++ nhom-5/js/text.js
@@ -1,21 +1,30 @@
 var data = "input.txt";
 var arr = [];
 var count = 0;
+var flipped = false;//để lật thẻ về mặt trước mối khi bấm continue hoặc load
 function flip() {
+	flipped = !flipped;
     $('.card').toggleClass('flipped');
 }
 function change(){
+	if(flipped) {
+		flip();
+	}
 	if(count >= 4) count = 0;
 	else count++;
 	$('.card .front').find("span").text(arr[count].word);
 	$('.card .back').find("span").text(arr[count].mean);
 }
 function load(){
+	count  = 0;
+	if(flipped) {
+		flip();
+	}
 	//var reader = new FileReader();
 	for(var i = 0; i < 5; i++){
 		var ele = {
-			word : ""+i,
-			mean : ""+i+5
+			word : "",
+			mean : ""
 		};
 		arr[i] = ele;
 	}
