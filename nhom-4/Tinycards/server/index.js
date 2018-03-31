9901937451dab0ed9f8c00125b4a260f5591d144
jindojojin <tranquanglinh.pt@gmail.com>
2018-03-31 12:03:55 -0400

    thu deploy co PORT

--- nhom-4/Tinycards/server/index.js
+++ nhom-4/Tinycards/server/index.js
@@ -2,9 +2,8 @@ const express = require('express')
 
 const app = express();
 const jsonParser= require('body-parser').json();
-app.get('/', (req, res)=> res.send('Hello'));
-app.listen(3000, () => console.log('server dang chay'));
-
+app.listen(process.env.PORT || 3000, () => console.log('server dang chay'));
+app.get('/i', (req, res)=> res.send('Hello'));
 app.post('/signin',jsonParser, (req, res) => {
     res.send(req.body);
 })
