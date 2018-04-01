8f2b606a79e642940f00ce497d21c0836b59735a
jindojojin <tranquanglinh.pt@gmail.com>
2018-04-01 07:37:58 -0400

    xoa bo server

--- nhom-4/Tinycards/server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express();
-const jsonParser= require('body-parser').json();
-app.listen(process.env.PORT || 3000, () => console.log('server dang chay'));
-app.get('/i', (req, res)=> res.send('Hello'));
-app.post('/signin',jsonParser, (req, res) => {
-    res.send(req.body);
-})
