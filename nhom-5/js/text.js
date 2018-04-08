324afa0aaecc9b5933eec3a201e07d34c057cb25
trinhtl <tonglytrinh@gmail.com>
2018-04-08 22:55:51 +0700

    fix

--- nhom-5/js/text.js
+++ nhom-5/js/text.js
@@ -1,17 +1,17 @@
 var data = "inputText.txt";
 var arr = [];//mang cac bo tu
-var count = 0;
+var count = 1;
 var flipped = false;//để lật thẻ về mặt trước mối khi bấm continue hoặc load
 var ranId = 0;//chi so mang arr
 var ranEle = 0;//word hay mean
 var question = "";
 var result = 0;
-
+var loaded = false;
 //chon bo tu
 function generate(){
 	console.log("generate");
 	ranId = Math.floor((Math.random()*4)+0);
-	ranEle = Math.floor((Math.random()*1)+0);
+	ranEle = Math.floor((Math.random()*5)+1);
 	if(ranEle == 1) question = arr[ranId].word;
 	else question = arr[ranId].mean;
 	document.getElementById('learnCard').innerHTML = question;
@@ -40,7 +40,7 @@ function check(){
 			result++;
 			if(ranId == 4) ranId = 0;
 			else ranId++;
-			ranEle = Math.floor((Math.random()*1)+0);
+			ranEle = Math.floor((Math.random()*5)+1);
 		}
 	} else {
 		if(cont == arr[ranId].word){
@@ -48,24 +48,27 @@ function check(){
 			result++;
 			if(ranId == 4) ranId = 0;
 			else ranId++;
-			ranEle = Math.floor((Math.random()*1)+0);
+			ranEle = Math.floor((Math.random()*5)+1);
 		}
 	}
 	
 	if(ranEle == 1) question = arr[ranId].word;
 	else question = arr[ranId].mean;
+	
+	$(".input").disabled = true;
+	
+	$('input').val("");
+	// setTimeout(function() {
+	// 	if(flag){
+	// 	// alert("Great");
+	// 		$("#anounce").text("Great");
+	// 	}else{
+	// 		$("#anounce").text("Wrong");
+	// 	}
+	// 	$("anounce").text("Check");
+	// },2000);
 	document.getElementById('learnCard').innerHTML = question;
 	console.log(document.getElementById('learnCard').innerHTML);
-	$('input').disabled = true;
-	if(flag){
-		alert("Great");
-	}else{
-		alert("Wrong");
-	}
-	setTimeout(function() {
-		$('input').val("");
-	},200);
-	// $('button#anounce').find("span").text("Check...");
 	if(result == 5) alert("You are Excellent, the Challenge is completed");
 }
 
@@ -80,16 +83,20 @@ function change(){
 	if(flipped) {
 		flip();
 	}
-	if(count >= 4) count = 0;
-	else count++;
-	$('.card .front').find("span").text(arr[count].word);
-	$('.card .back').find("span").text(arr[count].mean);
+	if(loaded==false) load();
+	else{
+		if(count >= 4) count = 0;
+		else count++;
+		$(".card .front").html(arr[count].word);
+		$(".card .back").html(arr[count].mean);
+	}
 }
 
 //khoi tao mang cac bo tu
 function load(){
-
-	count  = 0;
+	loaded = true;
+	console.log("load");
+	count  = 1;
 	if(flipped) {
 		flip();
 	}
@@ -111,7 +118,8 @@ function load(){
 	arr[3].mean = "hát";
 	arr[4].word = "beautiful";
 	arr[4].mean = "đẹp";
-	$('.card .front').find("span").text(arr[count++].word);
-	$('.card .back').find("span").text(arr[count++].mean);
+	$(".card .front").html(arr[count].word);
+	$(".card .back").html(arr[count].mean);
+	count++;
 }
 
